a4ea3d7a43e197bcc7c11e20ebc47263339cdbfd
Mohamed Abdelnasser <peskohere@gmail.com>
2026-05-01 15:36:47 +0300

    Highlighted Searching Words

--- src/components/ResultSearchComponent.jsx
+++ src/components/ResultSearchComponent.jsx
@@ -12,10 +12,9 @@ export default function ResultSearchComponent({ valSearchInput }) {
         const res = await axios.get(
           `https://localhost:7012/api/Search?inputValue=${valSearchInput}`,
         );
-        console.log(res.data);
+
         const rawData = res.data;
         const allUrls = Object.values(rawData).flat();
-
         const uniqueUrls = [...new Set(allUrls)];
 
         setData(uniqueUrls);
@@ -29,25 +28,39 @@ export default function ResultSearchComponent({ valSearchInput }) {
 
   if (!valSearchInput || data.length === 0) return null;
 
+  // ! Handling Links
+  const highlightQuery = encodeURIComponent(
+    valSearchInput.trim().split(/\s+/).join(","),
+  );
+
   return (
     <div className="resultComponent">
       <div className="content">
         <ul>
-          {data.map((url, index) => (
-            <li key={index} className="text-start flex items-center">
-              <a href={url} target="_blank" className="flex items-center gap-2">
-                <div className="img">
-                  <img
-                    className="w-8 h-8"
-                    src="./resultIcon.svg"
-                    alt="result-icon"
-                  />
-                </div>
-                <p>{url.length > 50 ? url.slice(0, 50) + "..." : url}</p>
-                {/* I repaired max length */}
-              </a>
-            </li>
-          ))}
+          {data.map((url, index) => {
+            const finalUrl = `${url}?highlight=${highlightQuery}`;
+
+            return (
+              <li key={index} className="text-start flex items-center">
+                <a
+                  href={finalUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-2"
+                >
+                  <div className="img">
+                    <img
+                      className="w-8 h-8"
+                      src="./resultIcon.svg"
+                      alt="result-icon"
+                    />
+                  </div>
+
+                  <p>{url.length > 50 ? url.slice(0, 50) + "..." : url}</p>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
